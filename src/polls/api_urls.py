5cfd8fb9ed866a36f2b28aa73f6e820eeb2b8d2a
Juan Arriagada <7169909+jarriagadac@users.noreply.github.com>
2025-08-02 14:11:35 +0000

    limpieza

--- src/polls/api_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .api_views import PollDetail, PollList
-
-urlpatterns = [
-    path("", PollList.as_view(), name="poll-list"),
-    path("<int:pk>/", PollDetail.as_view(), name="detail"),
-]
